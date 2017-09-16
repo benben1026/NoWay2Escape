00ba56674307eae90ad362d795f7cd026c4f49b4
benben1026 <zhouwei1026@gmail.com>
2017-09-16 13:34:48 -0700

    Add turning on/off engine function and accelerating function

--- Assets/script/car.cs
+++ Assets/script/car.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class car : MonoBehaviour {
-    public float turningAngle;
+	public float turningAngle;
+	public const int ACCELERATE_TIME = 30;
+	public int accelerateTimeLeft;
+
     private Rigidbody2D rigi;
 	private Vector2 previousVelocity;
-	private bool engineFlag = true;
+	private bool engineFlag;
+	private bool accelerateFlag;
+	private int accelerateCountdown;
     
     private void Awake()
     {
@@ -15,9 +20,13 @@ public class car : MonoBehaviour {
     
     // Use this for initialization
     void Start () {
-        this.rigi.velocity = new Vector2(0f, 1.5f);
+        this.rigi.velocity = new Vector2(0f, 0.5f);
 		this.previousVelocity = this.rigi.velocity;
         this.turningAngle = 0.02f; // Radian
+		this.engineFlag = true;
+		this.accelerateFlag = false;
+		this.accelerateCountdown = ACCELERATE_TIME;
+		this.accelerateTimeLeft = 3;
     }
 	
 	// Update is called once per frame
@@ -25,7 +34,7 @@ public class car : MonoBehaviour {
         float move = Input.GetAxis("Horizontal");
         print(rigi.velocity.magnitude);
 
-        if (move == 1)
+		if (move == 1 && this.engineFlag && !this.accelerateFlag)
         {
             float x = this.rigi.velocity.x;
             float y = this.rigi.velocity.y;
@@ -33,7 +42,7 @@ public class car : MonoBehaviour {
             this.rigi.velocity = new Vector2(x * Mathf.Cos(angle) + y * Mathf.Sin(angle), (-1) * x * Mathf.Sin(angle) + y * Mathf.Cos(angle));
             transform.Rotate(0, 0, (-1) * angle * 180 / Mathf.PI);
         }
-        else if (move == -1)
+		else if (move == -1 && this.engineFlag && !this.accelerateFlag)
         {
             float x = this.rigi.velocity.x;
             float y = this.rigi.velocity.y;
@@ -42,13 +51,25 @@ public class car : MonoBehaviour {
             transform.Rotate(0, 0, (-1) * angle * 180 / Mathf.PI);
         }
 
-		if (Input.GetKeyDown ("s") && this.engineFlag) {
+		if (Input.GetKeyDown ("s") && this.engineFlag && !this.accelerateFlag) {
 			this.previousVelocity = this.rigi.velocity;
 			this.rigi.velocity = new Vector2 (0, 0);
 			this.engineFlag = !this.engineFlag;
-		} else if (Input.GetKeyDown ("s")) {
+		} else if (Input.GetKeyDown ("s") && !this.accelerateFlag) {
 			this.rigi.velocity = this.previousVelocity;
 			this.engineFlag = !this.engineFlag;
+		}
+
+		if (Input.GetKeyDown ("a") && !this.accelerateFlag && this.accelerateTimeLeft > 0) {
+			this.accelerateFlag = true;
+			this.rigi.velocity = 8 * this.rigi.velocity;
+			this.accelerateTimeLeft--;
+		} else if (this.accelerateFlag && this.accelerateCountdown > 0) {
+			this.accelerateCountdown--;
+		} else if (this.accelerateFlag) {
+			this.accelerateFlag = false;
+			this.accelerateCountdown = ACCELERATE_TIME;
+			this.rigi.velocity = this.rigi.velocity / 8;
 		}
         
     }
