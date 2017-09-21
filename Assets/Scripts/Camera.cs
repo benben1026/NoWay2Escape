afa75a0aa50620cf4ac225d5c7ca443b4a053869
benben1026 <zhouwei1026@gmail.com>
2017-09-21 10:04:52 -0700

    Making the camera following the car

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	public GameObject car;
+
+	private Vector3 offset;
+	void Start () {
+		offset = transform.position - car.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = car.transform.position + offset;
+	}
+}
