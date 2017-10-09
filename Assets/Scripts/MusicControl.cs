0113fe607655ab60e219f1ce83a017aab5472270
zhixinzhang <zhangzhixin1992@gmail.com>
2017-10-09 12:56:30 -0700

    Add menu

---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MusicControl : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource myMusic;
+
+    public void VolumeControl()
+    {
+        myMusic.volume = Volume.value;
+    }
+}
---
+++ Assets/Scripts/SelectOutInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+public class SelectOutInput : MonoBehaviour {
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update() {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false) {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable() {
+        buttonSelected = false;
+    }
+	
+}
