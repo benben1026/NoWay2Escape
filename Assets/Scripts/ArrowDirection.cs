ed38c365540ef6bf46dce5684651bed810db8cb3
Guanglei Deng <denggl58@gmail.com>
2017-10-12 10:05:52 -0700

    replace the car sprite; add direction arrow

---
+++ Assets/Scripts/ArrowDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowDirection : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 relativePos = target.position - transform.position;
+        //Quaternion rotation = Quaternion.LookRotation(relativePos);
+        //transform.rotation = rotation;
+        transform.right = target.position - transform.position;
+    }
+}
